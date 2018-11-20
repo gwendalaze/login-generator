257a2426d6a52a386a7db7c6f7e127cd2fd836da
nrg2293a <gwendaleynard@gmail.com>
2018-11-20 11:52:46 +0100

    #2 Creation test

---
+++ src/test/java/geco/LoginGeneratorTest.java
@@ -0,0 +1,25 @@
+package geco;
+
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+public class LoginGeneratorTest {
+
+    private LoginService ls;
+
+    private LoginGenerator lg;
+
+    @Before
+    public void setUp(){
+        ls = new LoginService(new String []{"JROL","BPER","CGUR","JDU","JRAL","JRAL1"});
+        lg = new LoginGenerator(ls);
+    }
+
+    @Test
+    public void CT1(){
+        lg.generateLoginForNomAndPrenom("Durand","Paul");
+        assertTrue(ls.loginExists("PDUR"));
+    }
+
+}
